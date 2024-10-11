8579b903181ce22299784901f47a341b4df13ab7
nof1eld <imadeddine799@gmail.com>
2024-10-11 16:35:01 +0100

    Quartz sync: Oct 11, 2024, 4:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Study With Me",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
